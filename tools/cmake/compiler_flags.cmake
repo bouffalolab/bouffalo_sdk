97760e3633d7ce0f435be1d98e7c9e8c46bfaca7
cooldome <ariabushenko@gmail.com>
2021-07-28 20:26:56 +0100

    handle STREQUAL when variable is not defined

--- tools/cmake/compiler_flags.cmake
+++ tools/cmake/compiler_flags.cmake
@@ -9,7 +9,7 @@ list(APPEND GLOBAL_LD_FLAGS -Wall -Wchar-subscripts -std=c99)
 list(APPEND GLOBAL_LD_FLAGS --specs=nano.specs)
 
 
-if(${SUPPORT_FLOAT} STREQUAL "y")
+if("${SUPPORT_FLOAT}" STREQUAL "y")
 list(APPEND GLOBAL_LD_FLAGS -u _printf_float)
 endif()
 
