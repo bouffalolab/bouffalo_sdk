7c8010e3d1fb86e15c8c24075072cdafbf3c77bc
jzlv <jzlv@bouffalolab.com>
2022-10-24 19:19:45 +0800

    [fix][shell] fix uart macro

--- examples/shell/main.c
+++ examples/shell/main.c
@@ -9,16 +9,16 @@ static struct bflb_device_s *uart0;
 void uart_isr(int irq, void *arg)
 {
     uint32_t intstatus = bflb_uart_get_intstatus(uart0);
-    if (intstatus & UART_URX_FIFO_INT) {
+    if (intstatus & UART_INTSTS_RX_FIFO) {
         while (bflb_uart_rxavailable(uart0)) {
             shell_handler(bflb_uart_getchar(uart0));
         }
     }
-    if (intstatus & UART_URX_RTO_INT) {
+    if (intstatus & UART_INTSTS_RTO) {
         while (bflb_uart_rxavailable(uart0)) {
             shell_handler(bflb_uart_getchar(uart0));
         }
-        bflb_uart_int_clear(uart0, UART_CR_URX_RTO_CLR);
+        bflb_uart_int_clear(uart0, UART_INTCLR_RTO);
     }
 }
 
