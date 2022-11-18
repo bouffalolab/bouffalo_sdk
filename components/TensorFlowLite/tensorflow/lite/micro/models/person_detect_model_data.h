a65f86a7130fd608c4ea5f9df1f41030d3fdee4f
jzlv <jzlv@bouffalolab.com>
2022-11-18 16:06:20 +0800

    [feat] add tensorflowlite component and demo

---
+++ components/TensorFlowLite/tensorflow/lite/micro/models/person_detect_model_data.h
@@ -0,0 +1,2 @@
+extern const unsigned char g_person_detect_model_data[];
+extern const unsigned int g_person_detect_model_data_size;
---
+++ components/TensorFlowLite/tensorflow/lite/micro/models/vww2_50_50_INT8_model_data.h
@@ -0,0 +1,2 @@
+extern const unsigned char g_vww2_50_50_INT8_model_data[];
+extern const unsigned int g_vww2_50_50_INT8_model_data_size;
