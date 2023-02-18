54fc16fa9d8b376a966073fc750715d24bba28af
Roman Valls Guimera <brainstorm@users.noreply.github.com>
2023-02-18 22:59:11 +1100

    Add blinky for the M0S Dock as mentioned in https://github.com/sipeed/M0S_BL616_example/issues/2

--- examples/helloworld/main.c
+++ examples/helloworld/main.c
@@ -1,14 +1,31 @@
 #include "bflb_mtimer.h"
+#include "bflb_gpio.h"
 #include "board.h"
 
 #define DBG_TAG "MAIN"
 #include "log.h"
 
+struct bflb_device_s *gpio;
+
 int main(void)
 {
     board_init();
 
+    gpio = bflb_device_get_by_name("gpio");
+    printf("gpio output\r\n");
+    bflb_gpio_init(gpio, GPIO_PIN_27, GPIO_OUTPUT | GPIO_PULLUP | GPIO_SMT_EN | GPIO_DRV_0);
+    bflb_gpio_init(gpio, GPIO_PIN_28, GPIO_OUTPUT | GPIO_PULLUP | GPIO_SMT_EN | GPIO_DRV_0);
+
     while (1) {
+        bflb_gpio_set(gpio, GPIO_PIN_27);
+        bflb_gpio_reset(gpio, GPIO_PIN_28);
+        bflb_mtimer_delay_ms(500);
+
+        bflb_gpio_reset(gpio, GPIO_PIN_27);
+
+        bflb_gpio_set(gpio, GPIO_PIN_28);
+        bflb_mtimer_delay_ms(500);
+
         LOG_F("hello world fatal\r\n");
         LOG_E("hello world error\r\n");
         LOG_W("hello world warning\r\n");
