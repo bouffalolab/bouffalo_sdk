942bcec6fe63f69dbac801b902c7e0a305d05d9c
jzlv <jzlv@bouffalolab.com>
2023-03-09 17:42:00 +0800

    [update][board] move kmem_init before print

--- bsp/board/bl702dk/board.c
+++ bsp/board/bl702dk/board.c
@@ -236,18 +236,18 @@ void board_init(void)
 
     console_init();
 
-    bl_show_log();
-    if (ret != 0) {
-        printf("flash init fail!!!\r\n");
-    }
-    bl_show_flashinfo();
-
 #ifdef CONFIG_PSRAM
     board_psram_init();
 #endif
 
     size_t heap_len = ((size_t)&__HeapLimit - (size_t)&__HeapBase);
     kmem_init((void *)&__HeapBase, heap_len);
+
+    bl_show_log();
+    if (ret != 0) {
+        printf("flash init fail!!!\r\n");
+    }
+    bl_show_flashinfo();
 
     printf("dynamic memory init success,heap size = %d Kbyte \r\n", ((size_t)&__HeapLimit - (size_t)&__HeapBase) / 1024);
 
